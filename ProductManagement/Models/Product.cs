66f7629154c30602278f4912c6f8c6da5c7a13f4
Dursun <dursunpekcan7@gmail.com>
2025-12-16 15:06:02 +0300

    Proje dosyası ekle.

---
+++ ProductManagement/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }          // Primary Key
+        public string Name { get; set; }     // Ürün adı
+        public decimal Price { get; set; }   // Fiyat
+        public string Description { get; set; } // Açıklama
+        public DateTime CreatedDate { get; set; } // Eklenme tarihi
+    }
+}
